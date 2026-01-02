7d80786e621c2266db0773cdedead44dabbe3260
Kasun Sagara <kasunsagara689@gmail.com>
2026-01-02 16:35:35 +0530

    create login function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,12 @@
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 export function createUser(req, res) {
-    const user = new User(req.body)
+    const newUserData = req.body
+
+    newUserData.password = bcrypt.hashSync(newUserData.password, 10)
+
+    const user = new User(newUserData)
     user.save().then(
         () => {
             res.json({
@@ -13,6 +18,31 @@ export function createUser(req, res) {
             res.json({
                 message: "User not created"
             })
+        }
+    )
+}
+
+export function loginUser(req, res) {
+    User.findOne({email: req.body.email}).then(
+        user => {
+            if(!user) {
+                res.json({
+                    message: "User not found"
+                })
+            } else {
+
+                const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password)
+
+                if(isPasswordCorrect) {
+                    res.json({
+                        message: "User logged in"
+                    })
+                } else {
+                    res.json({
+                        message: "User not logged in (wrong password)"
+                    })
+                }
+            }
         }
     )
 }
