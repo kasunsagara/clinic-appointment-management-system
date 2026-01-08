fb6ea0bdff73176ac97379f5178ad7d369b9a496
Kasun Sagara <kasunsagara689@gmail.com>
2026-01-08 11:32:08 +0530

    small change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,18 @@ import HomePage from './pages/homePage';
 import LoginPage from './pages/loginPage';
 import AdminHomePage from './pages/adminHomePage';
 
-
 function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <BrowserRouter> 
     <Routes>
     <Route path="/" element={<HomePage/>}/>
     <Route path="/login" element={<LoginPage/>}/>
-    <Route path="/admin/*" element={<AdminHomePage/>}/>
+    <Route path="/admin/*" element={<AdminHomePage/>}/> 
     <Route path="/*" element={<h1>404 error</h1>}/>
     </Routes>
-    </BrowserRouter>
+    </BrowserRouter> 
     </>
   )
 }
