015e568b439b158819972842ecd86438171f9525
Kasun Sagara <kasunsagara689@gmail.com>
2026-04-04 16:20:30 +0530

    small change

--- frontend/src/pages/admin/addDoctorPage.jsx
+++ frontend/src/pages/admin/addDoctorPage.jsx
@@ -204,6 +204,15 @@ export default function AddDoctorPage() {
                 onChange={(e) => setProfileImageFile(e.target.files[0])}
                 className="w-full px-3 py-2 border rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
               />
+
+              {/* ✅ preview */}
+              {profileImageFile && (
+                <img
+                  src={URL.createObjectURL(profileImageFile)}
+                  alt="Preview"
+                  className="mt-2 max-w-xs h-auto border rounded-lg"
+                />
+              )}
             </div>
           </div>
 
