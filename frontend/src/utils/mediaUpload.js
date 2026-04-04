22e48c75874d13b7a63cd14423d109f4ea573417
Kasun Sagara <kasunsagara689@gmail.com>
2026-04-04 10:49:58 +0530

    add supabase media upload file

---
+++ frontend/src/utils/mediaUpload.js
@@ -0,0 +1,31 @@
+import { createClient } from "@supabase/supabase-js";
+
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRod2d4cmFrYnNpY3Zsc2pybHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyNzQxODIsImV4cCI6MjA5MDg1MDE4Mn0.0Lw2oeN34BIHmSvJUurOTiGhQOcqDnR6zFDGjoryahA`;
+
+const url = "https://dhwgxrakbsicvlsjrlwp.supabase.co";
+
+const supabase = createClient(url, key);
+
+export default function uploadMediaToSupabase(file) {
+  return new Promise((resolve, reject) => {
+    if (file == null) {
+      reject("File not added");
+    }
+    let fileName = file.name;
+    const extension = fileName.split(".")[fileName.split(".").length - 1];
+
+    const timestamp = new Date().getTime();
+
+    fileName = timestamp +file.name+ "." + extension;
+
+    supabase.storage.from("profile-images").upload(fileName, file, {
+      cacheControl: "3600",
+      upsert: false,
+    }).then(()=>{
+      const publicUrl = supabase.storage.from("profile-images").getPublicUrl(fileName).data.publicUrl;
+      resolve(publicUrl);
+    }).catch((err)=>{
+      reject(err);
+    });
+  });
+}
